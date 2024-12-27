9b5cf095d29fcdf304d5adcde6b2f3219ea12178
Soham-Debnath <20debnathsoham04@gmail.com>
2024-12-28 00:30:10 +0530

    01_loops

---
+++ 01_loops.py
@@ -0,0 +1,18 @@
+# Print 1 to 10
+
+# For loop
+for i in range (11):
+    print(i, end=" ")
+print("\n")
+# While loop
+a=0
+while(a<11):
+    print(a,end=" ")
+    a+=1
+print("\n")
+
+# Loop in list
+
+l=[1,"garry","virat","rohit","soham",69]
+for i in range (len(l)):
+    print(l[i])
