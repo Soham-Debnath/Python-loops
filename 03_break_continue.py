737001f47535ac0d6034eac2404e2a3185bad7e5
Soham-Debnath <20debnathsoham04@gmail.com>
2024-12-28 00:32:26 +0530

    03_loops

---
+++ 03_break_continue.py
@@ -0,0 +1,13 @@
+# Break
+
+# for i in range(0,11):
+#     if(i==7):
+#         break   #exit just before 7
+#     print(i)
+ 
+# Continue
+  
+for p in range(0,11):
+    if(p==7):
+       continue  #skip 7
+    print(p)
