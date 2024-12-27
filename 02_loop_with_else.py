e01cc044882a0de40f17c9f11b33908a03e3e909
Soham-Debnath <20debnathsoham04@gmail.com>
2024-12-28 00:31:15 +0530

    02_loops

---
+++ 02_loop_with_else.py
@@ -0,0 +1,20 @@
+# Else acts when the loop exhausts.
+
+t=(1,7,69,"harry","rahul") # tuple
+
+# for i in t:
+#     print(i)
+# else:
+#     print("done\n")
+
+for i in range(len(t)):
+    print(t[i])
+else:
+    print("done")
+
+# a=0
+# while(a<len(t)):
+#     print(t[a])
+#     a+=1
+# else:
+#     print("done2")
